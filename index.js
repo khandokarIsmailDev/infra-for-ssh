4ecb00e03fef198b6a9a73954c3e9f644dbc6cb5
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-09-11 03:27:34 +0600

    create a vpc

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const pulumi = require("@pulumi/pulumi")
 const aws = require("@pulumi/aws")
 
+//create a vpc 
+const vpc = new aws.ec2.vpc("my-vpc", {
+    cidrBlock : "10.0.0.0/16",
+    tags :{
+        Name:"my-vpc"
+    }
+})
 
-
+exports.vpcId = vpc.id
 
 
